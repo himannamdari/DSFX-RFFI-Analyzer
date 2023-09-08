3cafdcb52986ff17e73fc69ba76ec1309ff989e3
Himan Namdari <himan.namdari@gmail.com>
2023-09-08 12:50:53 -0400

    Update ml.py
    THe ML.py will train an NN on raw, extracted and PCA feature set and compare their results.

--- ml.py
+++ ml.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.neural_network import MLPRegressor
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import time
 
@@ -45,12 +46,25 @@ def train_and_evaluate_nn(data_path, top_features):
     ext_nn.fit(X_train_scaled, y_train)
     ext_training_time = time.time() - start_time
 
+    # Apply PCA to the data with 2 components
+    pca = PCA(n_components=2)
+    pca_data = pca.fit_transform(X_train_scaled)
+
+    # Initialize and train a neural network using PCA data
+    pca_nn = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=42)
+    start_time = time.time()
+    pca_nn.fit(pca_data, y_train)
+    pca_training_time = time.time() - start_time
+
     # Predictions
     raw_train_preds = raw_nn.predict(X_train_scaled)
     raw_test_preds = raw_nn.predict(X_test_scaled)
 
     ext_train_preds = ext_nn.predict(X_train_scaled)
     ext_test_preds = ext_nn.predict(X_test_scaled)
+
+    pca_test_data = pca.transform(X_test_scaled)
+    pca_test_preds = pca_nn.predict(pca_test_data)
 
     # Calculate RMSE
     raw_train_rmse = np.sqrt(mean_squared_error(y_train, raw_train_preds))
@@ -59,13 +73,17 @@ def train_and_evaluate_nn(data_path, top_features):
     ext_train_rmse = np.sqrt(mean_squared_error(y_train, ext_train_preds))
     ext_test_rmse = np.sqrt(mean_squared_error(y_test, ext_test_preds))
 
+    pca_test_rmse = np.sqrt(mean_squared_error(y_test, pca_test_preds))
+
     return {
         'raw_train_rmse': raw_train_rmse,
         'raw_test_rmse': raw_test_rmse,
         'ext_train_rmse': ext_train_rmse,
         'ext_test_rmse': ext_test_rmse,
+        'pca_test_rmse': pca_test_rmse,
         'raw_training_time': raw_training_time,
-        'ext_training_time': ext_training_time
+        'ext_training_time': ext_training_time,
+        'pca_training_time': pca_training_time
     }
 
 # Example usage:
@@ -78,15 +96,16 @@ results = train_and_evaluate_nn(data_path, top_features)
 # Plot RMSE and Training Time
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-labels = ['Raw Train', 'Raw Test', 'Extracted Train', 'Extracted Test']
-rmse_values = [results['raw_train_rmse'], results['raw_test_rmse'], results['ext_train_rmse'], results['ext_test_rmse']]
-training_times = [results['raw_training_time'], results['ext_training_time']]
+labels = ['Raw Train', 'Raw Test', 'Extracted Train', 'Extracted Test', 'PCA Test']
+rmse_values = [results['raw_train_rmse'], results['raw_test_rmse'], results['ext_train_rmse'],
+               results['ext_test_rmse'], results['pca_test_rmse']]
+training_times = [results['raw_training_time'], results['ext_training_time'], results['pca_training_time']]
 
-ax1.bar(labels, rmse_values, color=['b', 'g', 'b', 'g'])
+ax1.bar(labels, rmse_values, color=['b', 'g', 'b', 'g', 'r'])
 ax1.set_ylabel('RMSE')
 ax1.set_title('RMSE Comparison')
 
-ax2.bar(['Raw', 'Extracted'], training_times, color=['b', 'g'])
+ax2.bar(['Raw', 'Extracted', 'PCA'], training_times, color=['b', 'g', 'r'])
 ax2.set_ylabel('Training Time (s)')
 ax2.set_title('Training Time Comparison')
 
